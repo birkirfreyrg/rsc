6e4e50e3d0cd801a4b9f59c90c79a584473c15bd
birkirfreyrg <birkirfrey@gmail.com>
2023-04-08 22:01:56 +0000

    Work in progress, html for left side done, needs a lot more css work.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Box-container'>
+          <div className='Result-box'>
+            <div className='Your-result-header'>Your Result</div>
+            <div className='Result-circle'>
+              <div className='Result-value'>76</div>
+              <div className='Result-total-value'>of 100</div>
+            </div>
+            <div className='Great-text'>Great</div>
+            <div className='Your-score-text'>You scored higher than 65% of the people who have taken these tests.</div>
+          </div>
+            
+          <div className='Summary-box'></div>
+        </div>
       </header>
     </div>
   );
