66debc538c264694fbb23354428479c7f1a05eb7
birkirfreyrg <birkirfrey@gmail.com>
2023-04-10 20:31:42 +0000

    Changed approach update.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-
+import React from 'react';
 import './App.css';
+
+const SummaryBox = () => {
+  return (
+    <div className='Summary-component '>
+      <div className='Summary-box'>
+        <div className='Summary-header'>Summary</div>
+      </div>
+    </div>
+  );
+};
+
+const ResultBox = () => {
+  return (
+        <div className='Result-box'>
+          <div className='Your-result-header'>Your Result</div>
+          <div className='Result-circle'>
+            <div className='Result-value'>76</div>
+            <div className='Result-total-value'>of 100</div>
+          </div>
+          <div className='Great-text'>Great</div>
+          <div className='Your-score-text'>You scored higher than 65% of the people who have taken these tests.</div>
+  </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className='Box-container'>
-          <div className='Result-box'>
-            <div className='Your-result-header'>Your Result</div>
-            <div className='Result-circle'>
-              <div className='Result-value'>76</div>
-              <div className='Result-total-value'>of 100</div>
-            </div>
-            <div className='Great-text'>Great</div>
-            <div className='Your-score-text'>You scored higher than 65% of the people who have taken these tests.</div>
-          </div>
-            
-          <div className='Summary-box'></div>
-        </div>
-      </header>
+    <div className='App'>
+      <div className='Box-container'>
+        <ResultBox />
+        <SummaryBox />
+      </div>
     </div>
   );
 }
