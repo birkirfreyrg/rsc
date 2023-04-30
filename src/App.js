3ab69ec32a775e4cf804ff8528bc246b34f8be04
birkirfreyrg <birkirfrey@gmail.com>
2023-04-30 22:27:20 +0000

    Updating project. Added images, text.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
+import ReactionIcon from "./images/icon-reaction.svg"
+import MemoryIcon from "./images/icon-memory.svg"
+import VerbalIcon from "./images/icon-verbal.svg"
+import VisualIcon from "./images/icon-visual.svg"
 
 const SummaryBox = () => {
   return (
     <div className='Summary-component '>
       <div className='Summary-box'>
         <div className='Summary-header'>Summary</div>
+        <div className='Summary-result-container1'>
+          <img alt="Reaction Icon image" src={ReactionIcon} className="Icon-image" /> 
+          <div className='Reaction-text'>Reaction</div>
+        </div>
+        <div className='Summary-result-container2'>
+          <img alt="Memory Icon image" src={MemoryIcon} className="Icon-image" /> 
+          <div className='Reaction-text'>Reaction</div>
+        </div>
+        <div className='Summary-result-container3'>
+          <img alt="Verbal Icon image" src={VerbalIcon} className="Icon-image" /> 
+          <div className='Reaction-text'>Reaction</div>
+        </div>
+        <div className='Summary-result-container4'>
+          <img alt="Visual Icon image" src={VisualIcon} className="Icon-image" /> 
+          <div className='Reaction-text'>Reaction</div>
+        </div>
+
+        <div className='Summary-button-container'>
+          <button className='Summary-button' >Continue</button>
+        </div>
       </div>
     </div>
   );
